c96efa2ea42340b0cd8c181214cee0383f6d3f53
Daniil <tdm290705@gmail.com>
2025-04-18 04:13:49 +0300

    add UserEntity

---
+++ ww-user-service/src/main/java/com/technokratos/model/UserEntity.java
@@ -0,0 +1,40 @@
+package com.technokratos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_info")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private UUID id;
+    private String username;
+    private String password;
+    private String name;
+    private String lastname;
+    private String email;
+    private String bio;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+}
