1f99b918d38fafdbd9dd0f501828689f7c56e33e
Daniil <tdm290705@gmail.com>
2025-04-20 00:48:46 +0300

    add verify

--- ww-user-service/src/main/java/com/technokratos/service/UserService.java
+++ ww-user-service/src/main/java/com/technokratos/service/UserService.java
@@ -3,22 +3,39 @@ package com.technokratos.service;
 import com.technokratos.model.UserEntity;
 import com.technokratos.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
    private final UserRepository userRepository;
-
-   private BCryptPasswordEncoder passwordEncoder;
+   private final AuthenticationManager authenticationManager;
+   private final BCryptPasswordEncoder passwordEncoder;
 
 
    public UserEntity register(UserEntity user) {
        user.setPassword(passwordEncoder.encode(user.getPassword()));
        userRepository.save(user);
+       log.info("user service register");
        return user;
    }
 
+    public String verify(UserEntity user) {
+        Authentication authentication =
+                authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                                                                        user.getUsername(),
+                                                                        user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return "success";
+        }
+
+        return "fail";
+    }
 }
