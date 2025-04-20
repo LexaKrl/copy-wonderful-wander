b48d8d5f67c39a90e58642a4f9550706ce373180
Daniil <tdm290705@gmail.com>
2025-04-21 00:22:06 +0300

    add return of token to client and fix of UUID generation on backend

--- ww-user-service/src/main/java/com/technokratos/service/auth/AuthUserService.java
+++ ww-user-service/src/main/java/com/technokratos/service/auth/AuthUserService.java
@@ -1,4 +1,4 @@
-package com.technokratos.service;
+package com.technokratos.service.auth;
 
 import com.technokratos.model.UserEntity;
 import com.technokratos.repository.UserRepository;
@@ -10,17 +10,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserService {
+public class AuthUserService {
 
    private final UserRepository userRepository;
    private final AuthenticationManager authenticationManager;
    private final BCryptPasswordEncoder passwordEncoder;
+   private final JWTService jwtService;
 
 
    public UserEntity register(UserEntity user) {
+       user.setId(UUID.randomUUID());
        user.setPassword(passwordEncoder.encode(user.getPassword()));
        userRepository.save(user);
        log.info("user service register");
@@ -33,9 +38,12 @@ public class UserService {
                                                                         user.getUsername(),
                                                                         user.getPassword()));
         if (authentication.isAuthenticated()) {
-            return "success";
+            return jwtService.generateToken(user);
         }
-
         return "fail";
+    }
+
+    public List<UserEntity> getAll() {
+       return userRepository.findAll();
     }
 }
