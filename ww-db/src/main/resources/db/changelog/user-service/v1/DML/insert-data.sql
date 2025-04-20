3b20f080f8b135e055079b1a14cd13ff825d6454
Daniil <tdm290705@gmail.com>
2025-04-21 00:35:04 +0300

    init dml user service

---
+++ ww-db/src/main/resources/db/changelog/user-service/v1/DML/insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_info(user_id, username, password, name, lastname, email, bio, avatar_url, created_at, updated_at)
+VALUES ('8431cd6b-3697-48b3-b63a-44980947b6f6', 'danyo_ok', '1234', 'Daniil', 'Tyushin', 'danya@example.com',
+        'Software engineer and open-source enthusiast.', 'https://example.com/avatars/john.jpg', now(), now());
