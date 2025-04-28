2daa083e7e1e36fe23a395b124da7d4400a4cd9b
Alex Kirilin <kirilin.75.ru@gmail.com>
2025-04-28 18:29:57 +0300

    Changed files into the version 1(implementation without webhook)

--- ww-api/src/main/java/com/technokratos/api/WalkApi.java
+++ ww-api/src/main/java/com/technokratos/api/WalkApi.java
@@ -1,5 +1,6 @@
 package com.technokratos.api;
 
+import com.technokratos.dto.request.LocationData;
 import com.technokratos.dto.request.WalkRequest;
 import com.technokratos.dto.response.WalkResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -33,23 +34,23 @@ public interface WalkApi {
     @Operation(
             summary = "Get all walks",
             description = "Get sorted page of walks",
-            tags = {"walks"}
+            tags = {"walks"},
+            responses = {
+                @ApiResponse(
+                    responseCode = "200",
+                    description = "Walks received",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = WalkResponse.class))
+                    )
+                ),
+                @ApiResponse(
+                    responseCode = "404",
+                    description = "Walks not found"
+                )
+            }
     )
     @GetMapping
-    @ApiResponse(
-            responseCode = "200",
-            description = "Walks received",
-            content = @Content(
-                    mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation = WalkResponse.class)),
-                    examples = @ExampleObject(
-                            value = "[{\"id\": \"550e8400-e29b-41d4-a716-446655440000\"}]")
-            )
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Walks not found"
-    )
     ResponseEntity<Page<WalkResponse>> getWalks(
             @Parameter(
                     description = "Pageable type with size and sort type",
@@ -65,17 +66,19 @@ public interface WalkApi {
     * */
 
     @Operation(
-            summary = "Create a walk",
-            description = "Create a walk",
-            tags = {"walks"}
-    )
-    @ApiResponse(
-            responseCode = "201",
-            description = "Walk created"
-    )
-    @ApiResponse(
-            responseCode = "400",
-            description = "Walk creation failed"
+        summary = "Create a walk",
+        description = "Create a walk",
+        tags = {"walks"},
+        responses = {
+            @ApiResponse(
+                responseCode = "201",
+                description = "Walk created"
+            ),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Walk creation failed"
+            )
+        }
     )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -96,21 +99,23 @@ public interface WalkApi {
     @Operation(
             summary = "Get a walk by its ID",
             description = "There is a walk_id in the path. So we are getting a walk by its ID",
-            tags = {"walks"}
+            tags = {"walks"},
+            responses = {
+                @ApiResponse(
+                    responseCode = "200",
+                    description = "Walk retrieved",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = WalkResponse.class)
+                    )
+                ),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Walk not found"
+                )
+            }
     )
     @GetMapping("/{walkId}")
-    @ApiResponse(
-            responseCode = "200",
-            description = "Walk retrieved",
-            content = @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = WalkResponse.class)
-            )
-    )
-    @ApiResponse(
-            responseCode = "400",
-            description = "Walk not found"
-    )
     ResponseEntity<WalkResponse> getWalk(
             @Parameter(
                     description = "UUID of the walk",
@@ -127,18 +132,20 @@ public interface WalkApi {
     @Operation(
             summary = "Delete a walk by its ID",
             description = "There is a path variable walk_id. So we take this and delete walk by its ID",
-            tags = {"walks"}
+            tags = {"walks"},
+            responses = {
+                @ApiResponse(
+                    responseCode = "204",
+                    description = "Walk successfully deleted"
+                ),
+                @ApiResponse(
+                    responseCode = "404",
+                    description = "Walk not found"
+                )
+            }
     )
     @DeleteMapping("/{walkId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiResponse(
-            responseCode = "204",
-            description = "Walk successfully deleted"
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Walk not found"
-    )
     void deleteWalk(
             @Parameter(
                     description = "UUID of the walk to delete",
@@ -155,22 +162,24 @@ public interface WalkApi {
     @Operation(
             summary = "Update a walk by its ID",
             description = "There is a path variable walk_id. So we take this and update walk by its ID",
-            tags = {"walks"}
+            tags = {"walks"},
+            responses = {
+                    @ApiResponse(
+                        responseCode = "204",
+                        description = "Walk successfully updated"
+                    ),
+                    @ApiResponse(
+                        responseCode = "400",
+                        description = "Invalid input data"
+                    ),
+                    @ApiResponse(
+                        responseCode = "404",
+                        description = "Walk not found"
+                    )
+            }
     )
     @PutMapping("/{walkId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiResponse(
-            responseCode = "204",
-            description = "Walk successfully updated"
-    )
-    @ApiResponse(
-            responseCode = "400",
-            description = "Invalid input data"
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Walk not found"
-    )
     void updateWalk(
             @Parameter(
                     description = "The body of the walk request that user sends to the server",
@@ -194,21 +203,23 @@ public interface WalkApi {
     @Operation(
             summary = "Upload photo to walk",
             description = "Upload photo binded to walk",
-            tags = {"walks"}
+            tags = {"walks"},
+            responses = {
+                @ApiResponse(
+                    responseCode = "200",
+                    description = "Photo uploaded successfully"
+                ),
+                @ApiResponse(
+                    responseCode = "404",
+                    description = "Walk not found"
+                ),
+                @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid file format or size"
+                ),
+            }
     )
     @PostMapping(value = "/{walkId}/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ApiResponse(
-            responseCode = "200",
-            description = "Photo uploaded successfully"
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = "Walk not found"
-    )
-    @ApiResponse(
-            responseCode = "400",
-            description = "Invalid file format or size"
-    )
     ResponseEntity<Void> uploadWalkPhoto(
             @Parameter(
                     description = "UUID of the walk",
@@ -222,6 +233,48 @@ public interface WalkApi {
                     content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
             )
             @RequestPart("file") MultipartFile file
+    );
+
+    /*
+    *   Record data using method
+    *
+    * */
+
+    @Operation(
+            summary = "Recording location data",
+            description = "Record location data from the user to the server",
+            tags = {"Walk recording"},
+            responses = {
+                @ApiResponse(
+                    responseCode = "202",
+                    description = "Location updated"
+                ),
+                @ApiResponse(
+                    responseCode = "404",
+                    description = "Walk not found"
+                ),
+                @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid data"
+                )
+            }
+    )
+    @PostMapping("/record/{walkId}")
+    void recordData(
+            @Parameter(
+                    description = "UUID of the walk which data is recording",
+                    example = "550e8400-e29b-41d4-a716-446655440000",
+                    required = true
+            )
+            @PathVariable String walkId,
+            @Parameter(
+                    description = "The location data",
+                    required = true,
+                    content = @Content(
+                            schema = @Schema(implementation = LocationData.class)
+                    )
+            )
+            @RequestBody LocationData location
     );
 
 }
