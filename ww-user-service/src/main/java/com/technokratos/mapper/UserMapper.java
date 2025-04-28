29b96f6f45f582ee5813ccc9c03923c809a54392
Daniil <tdm290705@gmail.com>
2025-04-28 17:57:40 +0300

    add dto mappers

--- ww-user-service/src/main/java/com/technokratos/mapper/UserMapper.java
+++ ww-user-service/src/main/java/com/technokratos/mapper/UserMapper.java
@@ -1,17 +1,32 @@
 package com.technokratos.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import com.technokratos.dto.request.UserRequest;
+import com.technokratos.dto.request.*;
+import com.technokratos.dto.response.UserLoginResponse;
 import com.technokratos.dto.response.UserResponse;
 import com.technokratos.model.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "id", ignore = true)
-    UserEntity toEntity(UserRequest userRequest);
 
     UserResponse toResponse(UserEntity userEntity);
 
+    UserForJwtTokenRequest toJwtUserInfo(UserEntity userEntity);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity userRegistrationRequestToUserEntity(UserRegistrationRequest userRegistrationRequest);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity userProfileUpdateRequestToUserEntity(UserProfileUpdateRequest userProfileUpdateRequest);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity adminUserUpdateRequestToUserEntity(AdminUserUpdateRequest adminUserUpdateRequest);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity userLoginRequestToUserEntity(UserLoginRequest userLoginRequest);
+
+    @Mapping(target = "id", ignore = true)
+    UserEntity passwordChangeRequestToUserEntity(PasswordChangeRequest passwordChangeReq);
 }
