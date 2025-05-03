434f714bef592036ef7fab6934e9e617bd6cdc10
Alex Kirilin <kirilin.75.ru@gmail.com>
2025-05-03 14:51:17 +0300

    update api to add sse format for streaming data to subscribers + update previous classes according new structure

---
+++ ww-api/src/main/java/com/technokratos/dto/response/walk/WalkDataResponse.java
@@ -0,0 +1,18 @@
+package com.technokratos.dto.response.walk;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record WalkDataResponse(
+        @Schema(description = "Longitude", example = "-77.0364")
+        Double longitude,
+
+        @Schema(description = "Latitude", example = "38.8951")
+        Double latitude,
+
+        @Schema(description = "information about steps passed since last timestamp", example = "180")
+        Integer steps,
+
+        @Schema(description = "Information about meters passed since last timestamp", example = "213")
+        Integer meters
+) {
+}
