48d45b47fb42ba5511ffb1ca1366393f5a5f610e
Daniil <tdm290705@gmail.com>
2025-05-05 12:01:51 +0300

    add comment and log

--- ww-user-service/src/main/java/com/technokratos/service/auth/MyUserDetailsService.java
+++ ww-user-service/src/main/java/com/technokratos/service/auth/MyUserDetailsService.java
@@ -5,6 +5,8 @@ import com.technokratos.model.UserPrincipal;
 import com.technokratos.repository.UserRepository;
 import com.technokratos.util.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,12 +14,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MyUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+
+    /*
+    Настроить кэширование, чтобы при запросе на /profile/me не ходить всегда в бд
+    */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadByUsername doing... {}", username);
         return new UserPrincipal(userMapper.accountToUserEntity(
                 userRepository
                         .findByUsername(username)
