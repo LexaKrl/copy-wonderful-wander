58ccc9d71a9fd51285f967be5cd4c358125304d8
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-11 21:06:32 +0300

    feat: add validation

--- ww-api/src/main/java/com/technokratos/api/UserApi.java
+++ ww-api/src/main/java/com/technokratos/api/UserApi.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 @RequestMapping("/api/users")
 public interface UserApi {
     @GetMapping("/me")
--- ww-api/src/main/java/com/technokratos/dto/request/user/UserRequest.java
+++ ww-api/src/main/java/com/technokratos/dto/request/user/UserRequest.java
@@ -8,10 +8,19 @@ import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record UserRequest(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
         String email,
+        @NotBlank(message = "First name is required")
+        @Length(min = 3, max = 64, message = "Firstname length should be between 3 to 64 characters")
         String firstname,
+        @NotBlank(message = "Lastname is required")
+        @Length(min = 3, max = 64, message = "Lastname length should be between 3 to 64 characters")
         String lastname,
+        @Length(max = 254, message = "Bio cannot exceed 254 characters")
         String bio,
+        @NotNull(message = "Photo visibility is required")
         PhotoVisibility photoVisibility,
+        @NotNull(message = "Walk visibility is required")
         WalkVisibility walkVisibility) {
 }
