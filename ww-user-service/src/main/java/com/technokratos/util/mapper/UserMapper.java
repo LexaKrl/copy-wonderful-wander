b337a20c30d4c6253b1c3a683aeb5ec563f4c965
Daniil <tdm290705@gmail.com>
2025-05-12 18:04:20 +0300

    add map account to jwtUserInfo

--- ww-user-service/src/main/java/com/technokratos/util/mapper/UserMapper.java
+++ ww-user-service/src/main/java/com/technokratos/util/mapper/UserMapper.java
@@ -25,6 +25,8 @@ public interface UserMapper {
 
     UserForJwtTokenRequest toJwtUserInfo(UserEntity userEntity);
 
+    UserForJwtTokenRequest toJwtUserInfo(Account account);
+
     UserEntity accountToUserEntity(Account account);
 
     @Mapping(target = "userId", ignore = true)
