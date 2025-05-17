83583fb471eb642acd994c64eea2f6b7796fe668
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-17 11:31:35 +0300

    test context load

---
+++ ww-user-service/src/test/java/com/technokratos/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technokratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(profiles = "test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
