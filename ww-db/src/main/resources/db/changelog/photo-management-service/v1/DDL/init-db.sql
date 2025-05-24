319c14ec48f87891bf59b91ddb8d62ecf51798fb
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-24 17:18:55 +0300

    feat: add migrations

---
+++ ww-db/src/main/resources/db/changelog/photo-management-service/v1/DDL/init-db.sql
@@ -0,0 +1,13 @@
+CREATE TYPE file_extension AS ENUM ('jpg', 'jpeg', 'png', 'gif', 'webp');
+
+CREATE TABLE file_stored_metadata
+(
+    file_id     UUID         NOT NULL,
+    owner_id    UUID         NOT NULL,
+    filename    VARCHAR(255) NOT NULL,
+    extension   file_extension,
+    size        DECIMAL,
+    upload_date TIMESTAMP DEFAULT NOW(),
+    -----------------------------------
+    CONSTRAINT file_id_pk PRIMARY KEY (file_id)
+);
