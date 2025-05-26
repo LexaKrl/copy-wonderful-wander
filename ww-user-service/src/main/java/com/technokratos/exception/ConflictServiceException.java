b3bdc04803d60dbead4e6a4f6899b37f95ce31e6
Daniil <tdm290705@gmail.com>
2025-05-27 01:23:44 +0300

    refactor: 401 -> 409 exception status code

---
+++ ww-user-service/src/main/java/com/technokratos/exception/ConflictServiceException.java
@@ -0,0 +1,9 @@
+package com.technokratos.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ConflictServiceException extends ServiceException{
+    public ConflictServiceException(String message) {
+        super(message, HttpStatus.CONFLICT);
+    }
+}
