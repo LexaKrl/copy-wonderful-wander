83c313ce6df6bf8c856265803b45e85e22dcd4db
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-29 01:24:03 +0300

    feat: add minio starter

--- ww-starter-minio/src/main/java/com/technokratos/config/properties/MinioProperties.java
+++ ww-starter-minio/src/main/java/com/technokratos/config/properties/MinioProperties.java
@@ -2,13 +2,14 @@ package com.technokratos.config.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @ConfigurationProperties(prefix = "minio")
 public class MinioProperties {
+    private Boolean enabled;
     private String url;
     private String accessKey;
     private String secretKey;
+    private String bucket;
+    private Integer presignedUrlExpiration;
 }
