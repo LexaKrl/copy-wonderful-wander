a934fe6fdc4f063e7a26cc6e161810419860ef3b
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-29 14:20:56 +0300

    refactor: change configuration annotation

--- ww-starter-minio/src/main/java/com/technokratos/config/MinioAutoConfiguration.java
+++ ww-starter-minio/src/main/java/com/technokratos/config/MinioAutoConfiguration.java
@@ -2,13 +2,13 @@ package com.technokratos.config;
 
 import com.technokratos.config.properties.MinioProperties;
 import io.minio.MinioClient;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@AutoConfiguration
+@Configuration
 @ConditionalOnProperty(prefix = "minio", name = "enabled", havingValue = "true", matchIfMissing = false)
 @EnableConfigurationProperties(MinioProperties.class)
 public class MinioAutoConfiguration {
