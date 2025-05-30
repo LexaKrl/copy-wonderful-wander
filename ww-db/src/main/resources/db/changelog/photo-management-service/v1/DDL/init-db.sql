8ae6196c324b4118e108f88850c4a5f5d61561be
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-30 17:29:51 +0300

    add file type column

--- ww-db/src/main/resources/db/changelog/photo-management-service/v1/DDL/init-db.sql
+++ ww-db/src/main/resources/db/changelog/photo-management-service/v1/DDL/init-db.sql
@@ -5,6 +5,7 @@ CREATE TABLE file_stored_metadata
     file_id     UUID         NOT NULL,
     owner_id    UUID         NOT NULL,
     filename    VARCHAR(255) NOT NULL,
+    file_type   VARCHAR(50),
     extension   file_extension,
     size        DECIMAL,
     upload_date TIMESTAMP DEFAULT NOW(),
