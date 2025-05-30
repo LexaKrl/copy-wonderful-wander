8d4eb0ca7f98b4590d4a2c76936765232f9c10ad
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-05-30 17:29:01 +0300

    refactor: change service exceptions

--- ww-photo-management-service/src/main/java/com/technokratos/exception/EmptyPhotoUploadException.java
+++
@@ -1,7 +0,0 @@
-package com.technokratos.exception;
-
-public class EmptyPhotoUploadException extends BadRequestException {
-    public EmptyPhotoUploadException() {
-        super("Image cannot be empty");
-    }
-}
