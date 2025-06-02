9d16bb5434a3fcbc52d6989f0666fdbfe3728bc8
Daniil <tdm290705@gmail.com>
2025-06-02 14:16:53 +0300

    add post category business logic

---
+++ ww-post-service/src/main/java/ru/itis/danyook/model/CategoryEntity.java
@@ -0,0 +1,19 @@
+package ru.itis.danyook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "category")
+public class CategoryEntity {
+    @Id
+    private Long categoryId;
+    private String name;
+}
