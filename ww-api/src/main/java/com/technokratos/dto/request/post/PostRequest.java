205c4e34af41b7f7549b09ac55fe5df816f2b056
Daniil <tdm290705@gmail.com>
2025-06-16 05:21:45 +0300

    feat: add validation + swagger annotation

--- ww-api/src/main/java/com/technokratos/dto/request/post/PostRequest.java
+++ ww-api/src/main/java/com/technokratos/dto/request/post/PostRequest.java
@@ -1,15 +1,20 @@
 package com.technokratos.dto.request.post;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "DTO для создания и обновления поста")
 public record PostRequest(
         @Schema(description = "Подпись к фотографии", example = "Это мы в Дубае))",
                 requiredMode = Schema.RequiredMode.REQUIRED)
+        @Max(value = 128, message = "Title length should be 128 less characters")
         String title,
         @Schema(description = "Уникальный идентификатор фотографии UUID, по которому из облака можно получить ссылку на фото",
                 example = "550e8400-e29b-41d4-a716-446655440000", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotNull(message = "Image id is required")
+        @NotBlank(message = "Image filename is required")
+        @Max(value = 128, message = "Image filename length should be less 128 characters")
         String imageFilename,
         @Schema(description = "Уникальный идентификатор категории", example = "234523423",
                 requiredMode = Schema.RequiredMode.REQUIRED)
