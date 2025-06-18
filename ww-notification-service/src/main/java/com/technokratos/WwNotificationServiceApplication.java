bd56a8ed064e7b1a77ea6d21091ea3257ff01651
Alex Kirilin <kirilin.75.ru@gmail.com>
2025-06-18 16:43:52 +0300

    add application class

--- ww-notification-service/src/main/java/com/technokratos/WwNotificationServiceApplication.java
+++ ww-notification-service/src/main/java/com/technokratos/WwNotificationServiceApplication.java
@@ -2,11 +2,13 @@ package com.technokratos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @EnableRetry
+@EnableFeignClients
 @SpringBootApplication
 public class WwNotificationServiceApplication {
 
